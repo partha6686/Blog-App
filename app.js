c00e8f9c8e34a0b9a59a403867a079038aab8348
Partha <parthasarathi6686@gmail.com>
2021-10-01 20:24:06 +0530

    add database

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 const express = require("express");
 const https = require("https");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const postComps = [];
+mongoose.connect('mongodb://localhost:27017/blogDB');
+
+const postSchema = new mongoose.Schema({
+  title:{
+    type:String,
+    required: [true, "No blog Specified."]
+  },
+  body:{
+    type:String,
+    required: [true, "Can't create a blog without body."]
+  }
+});
+
+const Post = mongoose.model("Post",postSchema);
+
 
 app.get("/",function(req,res){
-  res.render("home",{
-    homeStartingContent: homeStartingContent,
-    posts: postComps
+  Post.find(function(err,posts){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("home",{
+        homeStartingContent: homeStartingContent,
+        posts: posts
+      });
+    }
   });
 
 });
@@ -41,37 +60,36 @@ app.get("/compose",function(req,res){
 });
 
 app.get("/posts/:postName",function(req,res){
-  let requestedPost = _.lowerCase(req.params.postName);
-  postComps.forEach(function(postComp){
-    let postCompTitle = _.lowerCase(postComp.postTitle);
-    if(postCompTitle === requestedPost){
+  let requestedPostId = req.params.postName;
+  Post.findOne({_id: requestedPostId},function(err,result){
+    if(err){
+      console.log(err);
+    }else{
+      //console.log(result);
       res.render("post",{
-        postTitle: postComp.postTitle,
-        postBody: postComp.postBody
+        postTitle: result.title,
+        postBody: result.body
       });
     }
   });
-
 });
 
 app.post("/compose",function(req,res){
-  const link = "/posts/" + _.kebabCase(req.body.composeTitle);
-  let postComp = {
-    postTitle: req.body.composeTitle,
-    postBody: req.body.composePost,
-    postLink: link
-  };
-  postComps.push(postComp);
-  res.redirect("/");
+  const title = req.body.composeTitle;
+  const body = req.body.composePost;
+
+  const postDoc = new Post({
+    title: title,
+    body: body
+  });
+  postDoc.save(function(err,results){
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect("/");
+    }
+  });
 });
-
-
-
-
-
-
-
-
 
 
 app.listen(3000, function() {
